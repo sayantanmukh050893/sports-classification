8cb6dfc8a3402092d8e3f50a2104b3170b167986
Sayantan Mukhopadhyay <59133918+sayantanmukh050893@users.noreply.github.com>
2020-06-11 18:31:31 +0530

    fourth commit

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://drive.google.com/u/0/uc?id=1dHt3E5CphAIpM8svvhuWbNfFcuGN2TTj&export=download'
-export_file_name = 'sports_classifier.pkl'
+export_file_name = 'export.pkl'
 
 classes = ['basketball_player', 'boxer', 'cricketer', 'footballer']
 path = Path(__file__).parent
